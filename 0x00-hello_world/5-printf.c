c8711f5859eb5cbcbb129b0c6dc03ec32b3a7e96
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-08 13:45:14 -0700

    prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Print a string
- *
- *   * Description: use printf
- *
- *    * Return: 0
- *
- *     */
-
-
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
 
 int main(void)
-
 {
-
 		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+		return (0);
 }
