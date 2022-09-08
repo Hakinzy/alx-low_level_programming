c3ef246c13833b2355d3acbd60e93061e05043f3
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-08 13:52:12 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ *   
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
-
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
@@ -26,5 +18,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
-
 }
