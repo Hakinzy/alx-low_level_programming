22b9cf56d52ee77f3b7db4f8bd4e1459e408155f
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-08 13:48:26 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point
  *Description: prints "\"Programming is like building a multilingual puzzle"
- *    * Return: Always 0 (Success)
- *     */
+ * Return: 0
+ */
 
 int main(void)
 {
