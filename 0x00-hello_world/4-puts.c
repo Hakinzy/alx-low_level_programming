0d960ed51c133b6d5d7631755a660bfcc7a133a4
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-08 13:05:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -10,13 +9,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
