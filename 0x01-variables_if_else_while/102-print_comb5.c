d1cf889739fdb9b103b8b9fd5efc8f77b022ffaa
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-09 16:57:39 -0700

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,28 +12,28 @@ int ones2 = '0';
 for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 {
 for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
-{
+{			
 for (tens2 = '0'; tens2 <= '9'; tens2++)/* prints tens digit*/
 {
 for (ones2 = '0'; ones2 <= '9'; ones2++)/* prints ones digit*/
 {
 if ((ones < ones2) && (tens <= tens2))
 {
-putchar(tens);
-putchar(ones);
-putchar(' ');
-putchar(tens2);
-putchar(ones2);
-if (!(ones == '8' && tens == '9'))/*add comma and space*/
+	putchar(tens);
+	putchar(ones);
+	putchar(' ');
+	putchar(tens2);
+	putchar(ones2);
+if (!(ones == '8' && tens == '9'))/*addes comma and space*/
 {
-putchar(',');
-putchar(' ');
+	putchar(',');
+	putchar(' ');
 }
 }
 }
 }
 }
 }
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
