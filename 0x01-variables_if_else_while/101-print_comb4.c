27fffefb328a6f52bcd989a20f8a8e41f0dfa4bf
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-09 13:19:22 -0700

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main - Entry point, print 00 to 99 using putchar
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+int y;
+int z;
+for (x = '0'; x <= '9'; x++)
+{
+for (y = '0'; y <= '9'; y++)
+{
+for (z = '0'; z <= '9'; z++)
+{
+if (x < y && y < z)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+if (!(x == '7' && y == '8' && z == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
