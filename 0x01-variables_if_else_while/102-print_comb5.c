ca40772e726ae467c4c1ab17baab9986a4996728
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-09 20:23:39 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - prints all possible different combinations of two digits.
+* main - prints all possible different combinations of two digits
 * Return: Always 0 (Success)
 */
 int main(void)
