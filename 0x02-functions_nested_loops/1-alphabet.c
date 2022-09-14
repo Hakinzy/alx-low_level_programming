e966222a311d5e1e1d78669bf37b55ba847edd16
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-13 17:38:52 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints alphabet in lower casing
+ *
+ */
+
+ void print_alphabet(void);
+ {
+	 int ch;
+
+	 for (ch = 'a'; ch<= 'z'; **ch)
+		 _putchar(ch);
+	 -putchar('\n');
+ }
