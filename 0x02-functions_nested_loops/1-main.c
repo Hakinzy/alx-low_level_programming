2db70a3c766c7e84ca31d387d04ef4fcc97827bf
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-13 18:44:43 -0700

    alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 void print_alphabet(void);
 
 int main(void)
 {
-	    print_alphabet();
-	    return (0);
+print_alphabet();
+return (0);
 }
-
