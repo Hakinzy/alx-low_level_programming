d744f170da1de90a69ea6c4f94203a5d81a200bf
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-14 07:28:29 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
 
-for (count = 0; count < 50; count++)
+for (count = 0; count < 50; ++count)
 {
 sum = fib1 + fib2;
 printf("%lu", sum);
@@ -27,5 +27,6 @@ printf("\n");
 else
 printf(",");
 }
+
 return (0);
 }
