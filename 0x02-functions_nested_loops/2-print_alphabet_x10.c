acecbf3dea86e7545c84dc061ea566496375fe23
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-13 19:32:33 -0700

    2 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,9 +11,9 @@ void print_alphabet_x10(void)
 int line, ch;
 
 for (line = 0; line <= 9; ++line)
-  {
-for (ch = 'a'; ch <= 'z'; ++ch)					
+{
+for (ch = 'a'; ch <= 'z'; ++ch)
 _putchar(ch);
 _putchar('\n');
-									}
+}
 }
