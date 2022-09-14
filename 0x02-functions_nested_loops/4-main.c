a1ac5dfd131ac9072ee58df087061f392c40428f
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-13 20:10:02 -0700

    isalpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,4 +1,4 @@
-#iinclude "main.h"
+#include "main.h"
 
 /**
 * main - check the code.
@@ -10,13 +10,13 @@ int main(void)
 int r;
 
 r = _isalpha('H');
-_putchar(r + '0');
+_putchar(r +'0');
 r = _isalpha('o');
-_putchar(r + '0');
+_putchar(r +'0');
 r = _isalpha(108);
-_putchar(r + '0');
+_putchar(r +'0');
 r = _isalpha(';');
-_putchar(r + '0');
+_putchar(r +'0');
 _putchar('\n');
 return (0);
 }
