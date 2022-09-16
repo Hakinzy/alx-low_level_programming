c1d685f77f92fe3e88a13f0e00a944a0c3554e2d
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-16 06:26:13 -0700

    most

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,13 @@ void print_most_numbers(void)
 {
 int i;
 
-for (i = 0; i <= 9; i++)
+for (i = '0'; i <= '9'; i++)
 {
-if (i != 2 && i != 4)
-_putchar('0' + 1);
+if (i == '2' || i == '4')
+{
+continue;
+}
+_putchar(i);
 }
 _putchar('\n');
 }
