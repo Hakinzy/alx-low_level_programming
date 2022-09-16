f45f2b7da6a61c26f7aa09a4fbcd3f149412a5dc
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-15 21:53:28 -0700

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,17 @@
 
 void print_line(int n)
 {
-int a = 0;
+int i;
 
-while (a <= n && n == 0)
+if (n <= 0)
 {
-_putchar('_'), a++;
+_putchar('\n'),
 }
-_putchar('\n');
+
+else
+{
+for (i = 1; i <= n; ++i)
+{
+_purchar('_');
+}
 }
