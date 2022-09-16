313219d273b6ebea82a15dd981c4047c542c2126
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-15 18:42:09 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main-h"
+/**
+ * _isupper - write the code for upper casing character
+ * @c: interger variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+if  (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
