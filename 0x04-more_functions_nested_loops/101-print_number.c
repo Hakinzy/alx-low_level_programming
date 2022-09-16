5fd3a122a0747189b4bb344f109757e90be98126
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-16 15:37:28 -0700

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,16 @@
 void print_number(int n)
 {
 unsigned int num;
-num = n;
 
+num = n;
 if (n < 0)
 {
-_putchar('_');
+_putchar(45);
 num = -num;
 }
-
-if ((num / 10) > 0)
+if (num / 10)
+{
 print_number(num / 10);
-
+}
 _putchar((num % 10) + '0');
 }
