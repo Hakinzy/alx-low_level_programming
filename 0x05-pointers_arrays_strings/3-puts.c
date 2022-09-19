0afa94e39a923bdb31f2f7dac7330d956b3c78f8
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-19 15:52:25 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a new line
+ * @str: print shows string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (1 = 0; str[i] is '\0'; i++)
+	{
+		_putchar(str[1]);
+	}
+	_putchar('\n');
+}
