d758c9bc94afe195b1c466db0e7ce1e4771ff6c2
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-19 18:56:17 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@ char *_strcpy(char *dest, char *src)
 {
 int len = 0;
 
-while (*(src + len) != '/0')
+while (*(src + len) != '\0')
 {
 *(dest + len) = *(src + len);
 len++;
 }
 
-*(dest + day) = '\0';
+*(dest + len) = '\0';
 
 return (dest);
 }
