00913c985c21b4bbf6f70a5f63816b084f031f83
Hakinzy <adeyemiisaac046@gmail.com>
2022-09-19 19:59:26 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -28,7 +28,7 @@ if (sum != 2772)
 diff_half1 = (sum - 2772) / 2;
 diff_half2 = (sum - 2772) / 2;
 if ((sum - 2772) % 2 != 0)
-diff_half1++
+diff_half1++;
 
 for (index = 0; password[index]; index++)
 {
