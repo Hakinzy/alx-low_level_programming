09c551a4fa5e984a4eb89925ab8607482ab8db18
Hakinzy <adeyemiisaac046@gmail.com>
2022-10-06 13:55:07 -0700

    more_malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,7 +8,7 @@
 * @n: number of bytes
 * Return: NULL
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
