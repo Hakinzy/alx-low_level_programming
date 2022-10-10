9bbb2bef03bf4bc84cb1ee91141ff739f6c72a81
Hakinzy <adeyemiisaac046@gmail.com>
2022-10-10 12:12:45 -0700

    object_like_macros

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+/**
+* Defining a macro in a Header file 
+* object_like_macro - name of the macro is SIZE
+* 1024 is the value of the macro
+* Description: whenever SIZE is called, it will be replaced with the value 1024
+*/
+
+#define SIZE 1024 
