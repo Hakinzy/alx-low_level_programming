094405ada91e0c90629a03aa47dbcd59219d55f3
Hakinzy <adeyemiisaac046@gmail.com>
2022-10-12 15:15:49 -0700

    FunctionsPOinter

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,8 +3,9 @@
 /**
 * print_name - print name using pointer to function
 * @name: string to add
-*@f: pointer to functiom
-*Return: nothing
+* @f: pointer to functiom
+* Return: nothing
+*/
  
 void print_name(char *name, void (*f)(char *))
 {
