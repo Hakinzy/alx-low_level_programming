e716dd9cb633d36bc7e1cf93cbb18d5d03d6bc60
Hakinzy <adeyemiisaac046@gmail.com>
2022-10-31 14:59:22 -0700

    Function that read a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: name of the file to be read
+ * @letters: the numbers of letters it should read and print.
+ * Return: the actual number of letters it could read and print
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	char *print_field;
+	int file, read_file;
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return (w);
+	/*Create the buffer print_field*/
+	print_field = malloc(letters * sizeof(char));
+	if (!print_field)
+		return (0);
+
+	/*Open the file*/
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	/*Read the file and save in buffer*/
+	read_file = read(file, print_field, letters);
+	/*Write as standard output*/
+	write(STDOUT_FILENO, print_field, read_file);
+
+	close(file);
+	free(print_field);
+	return (read_file);
 }
